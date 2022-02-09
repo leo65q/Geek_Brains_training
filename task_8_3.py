326cf34da0e822ea6e730a1fd923c1d1df8b788b
leo65q <leo65q@yandex.ru>
2022-02-09 11:48:47 +0300

    Dz_Lesson_8

---
+++ task_8_3.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+
+def type_logger(level=0):
+    def logger(func):
+        @wraps(func)
+        def decor(*argv, **kwargs):
+            all_args = list(argv) + list(kwargs.values())
+            norm_res = func(all_args[0])
+            if level == 1:
+                print(", ".join([f"{x}:{type(x)}" for x in all_args]))
+            elif level == 2:
+                print(f"{func.__name__}:{type(func)}")
+                print(f"{func.__name__}({all_args[0]}):{type(norm_res)}")
+            return norm_res
+        return decor
+    return logger
+
+
+@type_logger(2)
+def calc_cube(x):
+    return x ** 3
+
+
+a = calc_cube(77)
+print(a)
